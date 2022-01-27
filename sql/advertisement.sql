0b0ccd302e6fcb85012a4f7dd9445d3a47008744
Bertalan Krisztian <krisztian652@gmail.com>
2022-01-27 08:20:17 +0100

    sql code update

--- sql/advertisement.sql
+++ sql/advertisement.sql
@@ -1,31 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 26, 2022 at 12:02 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `advertisement`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `advertisements`
---
 
 CREATE TABLE `advertisements` (
   `id` int(11) NOT NULL,
@@ -33,41 +14,41 @@ CREATE TABLE `advertisements` (
   `title` varchar(255) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
+INSERT INTO `advertisements` (`id`, `userid`, `title`) VALUES
+(1, 1, 'Selling my php knowledge.'),
+(2, 2, 'I want to buy php skills.'),
+(3, 6, 'Airpods for sale!'),
+(4, 4, 'Selling crocs for 300$');
 
 CREATE TABLE `users` (
   `id` int(10) NOT NULL,
   `name` varchar(255) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
---
--- Indexes for dumped tables
---
+INSERT INTO `users` (`id`, `name`) VALUES
+(1, 'Bob'),
+(2, 'James'),
+(3, 'Joe'),
+(4, 'Mcdonald'),
+(5, 'Daniel'),
+(6, 'Robertson');
 
---
--- Indexes for table `advertisements`
---
+
 ALTER TABLE `advertisements`
   ADD PRIMARY KEY (`id`),
   ADD KEY `userid` (`userid`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `advertisements`
---
+ALTER TABLE `advertisements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+
 ALTER TABLE `advertisements`
   ADD CONSTRAINT `advertisements_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
